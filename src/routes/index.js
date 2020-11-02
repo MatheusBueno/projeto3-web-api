c6df2cf5c450bcee5b9e3ad1ab11154461e68875
Bueno <matheusbueno1063@gmail.com>
2020-11-02 20:47:43 -0300

    feat: add engine in package.json

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const routes = express.Router();
 const UserController = require("../controllers/userController");
 const SessionController = require("../controllers/sessionController");
 
-routes.get("/", () => console.log("ola"));
+routes.get("/", (req) => req.json({ message: "Ola mundo" }));
 routes.post("/users", UserController.store);
 routes.post("/login", SessionController.store);
 
