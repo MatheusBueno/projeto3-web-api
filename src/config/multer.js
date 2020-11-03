971f4f276649dbc60414289a0b195bee9aca7fec
Bueno <matheusbueno1063@gmail.com>
2020-11-02 22:29:34 -0300

    fix: upload

--- src/config/multer.js
+++ src/config/multer.js
@@ -2,10 +2,10 @@ const multer = require("multer");
 const path = require("path");
 
 module.exports = {
-  dest: path.resolve(__dirname, "..", "..", "public", "uploads"),
+  dest: path.resolve(__dirname, "..", "public", "uploads"),
   storage: multer.diskStorage({
     destination: (req, res, cb) => {
-      cb(null, path.resolve(__dirname, "..", "..", "public", "uploads"));
+      cb(null, path.resolve(__dirname, "..", "public", "uploads"));
     },
     filename: (req, file, cb) => {
       const fileName = `${Date.now()}-${file.originalname}`;
