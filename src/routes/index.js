2ceb6484b2ba54d40bad07147751dfa0d469efbd
Bueno <matheusbueno1063@gmail.com>
2020-11-02 22:39:21 -0300

    fix: upload

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,8 +14,12 @@ routes.get("/", authMiddleware, adminMiddleware, (req, res) =>
 routes.post("/users", UserController.store);
 routes.post("/login", SessionController.store);
 routes.post("/upload", multer(multerConfig).single("file"), (req, res) => {
-  console.log('HERE');
-  res.json({ message: "Upload realizado com sucesso", filePath: filename });
+  const filename = req.file.filename;
+
+  res.json({
+    message: "Upload realizado com sucesso",
+    filePath: `https://projeto-web-3.herokuapp.com/public/uploads/${filename}`,
+  });
 });
 
 module.exports = routes;
