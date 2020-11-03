160f228c38a50b638415e2f7346f400751871b7f
Bueno <matheusbueno1063@gmail.com>
2020-11-02 23:08:18 -0300

    fix: image

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", routes);
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(process.env.PWD + '/public'));
 
 mongoose
   .connect(uri, {
